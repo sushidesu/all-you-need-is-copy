2d29191b247f342bbec82eaf58b7554c261d9695
sushidesu <hugemerge@gmail.com>
2022-12-31 18:36:45 +0900

    format一覧

--- src/options.tsx
+++ src/options.tsx
@@ -1,11 +1,37 @@
-import React, { useState, useCallback } from "react"
+import React, { useState, useCallback, useMemo, useEffect } from "react"
 import ReactDOM from "react-dom/client"
+import type { CopyFormat } from "./feature/copy/copy-format"
+import { CopyFormatRepository } from "./infra/copy/copy-format-repository"
 
 const Options = () => {
+  const copyFormatRepository = useMemo(() => new CopyFormatRepository(), [])
+
   const [count, setCount] = useState(0)
+  const [copyFormats, setCopyFormats] = useState<CopyFormat[]>([])
+
+  useEffect(() => {
+    let unmounted = false
+
+    const start = async () => {
+      const formats = await copyFormatRepository.getAll()
+      if (!unmounted) {
+        setCopyFormats(formats)
+      }
+    }
+    start()
+
+    return () => {
+      unmounted = true
+    }
+  }, [])
 
   return (
     <div>
+      <ul>
+        {copyFormats.map((fmt) => (
+          <li key={fmt.id}>{fmt.name}</li>
+        ))}
+      </ul>
       <button
         onClick={useCallback(() => {
           setCount((prev) => prev + 1)
