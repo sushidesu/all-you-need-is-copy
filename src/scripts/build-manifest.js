81d7e5234f5645e6c2c794808fa236e2fc4ff692
sushidesu <hugemerge@gmail.com>
2022-12-31 12:59:09 +0900

    build manifest.json

---
+++ src/scripts/build-manifest.js
@@ -0,0 +1,28 @@
+const path = require("path")
+const { readFile , writeFile, mkdir } = require("fs/promises")
+const { cwd } = require("process")
+
+const MANIFEST_FILE_NAME = "manifest.json"
+const DIST_DIR_NAME = "build"
+
+const main = async () => {
+  const currentDir = cwd()
+
+  // read and process manifest.json
+  const fileBuf = await readFile(path.join(currentDir, MANIFEST_FILE_NAME))
+  const manifest = JSON.parse(fileBuf.toString())
+  const { $schema, ...rest } = manifest
+
+  // write manifest.json
+  const distDir = path.join(currentDir, DIST_DIR_NAME)
+  try {
+    await mkdir(distDir)
+  } catch(_) {
+    // distDir already exists
+    // do nothing.
+  }
+  await writeFile(path.join(distDir, MANIFEST_FILE_NAME), JSON.stringify(rest, undefined, 2))
+  console.log(rest)
+}
+
+main()
